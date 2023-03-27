c894bed3722a32bacb1a285480ea21a53a06a48e
Eiji Toriki <ageForEngi@outlook.jp>
2023-03-27 22:04:09 +0900

    カテゴリ追加フォームの作成

--- 09_flask_app/test_db.py
+++ 09_flask_app/test_db.py
@@ -4,7 +4,7 @@ from company_blog.models import BlogCategory
 # db.drop_all()
 # db.create_all()
 
-for i in range(1, 71):
+for i in range(1, 81):
     c1 = BlogCategory(category=f'category{i}') 
     db.session.add(c1)
     db.session.commit()
