efb963f41e1e276e3f6e3a9c8374e71fdff4fd3e
Eiji Toriki <ageForEngi@outlook.jp>
2023-03-28 23:06:08 +0900

    カテゴリ変更ページの実装

--- 09_flask_app/company_blog/main/views.py
+++ 09_flask_app/company_blog/main/views.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, render_template, request, url_for, redirect, flash
-from flask_login import login_required
+from flask import Blueprint, render_template, request, url_for, redirect, flash, abort
+from flask_login import login_required, current_user
 from company_blog.models import BlogCategory
-from company_blog.main.forms import BlogCategoryForm
+from company_blog.main.forms import BlogCategoryForm, UpdateCategoryForm
 from company_blog import db
 
 main = Blueprint('main', __name__)
@@ -23,3 +23,32 @@ def category_maintenance():
         flash(form.errors['category'][0])
 
     return render_template('category_maintenance.html', blog_categories=blog_categories, form=form)
+
+
+@main.route('/<int:category_id>/update_category', methods=['GET', 'POST'])
+@login_required
+def update_category(category_id):
+    category = BlogCategory.query.get_or_404(category_id)
+    if not current_user.is_administrator():
+        abort(403)
+    form = UpdateCategoryForm(category_id)
+    if form.validate_on_submit():
+        category.category = form.category.data
+        db.session.commit()
+        flash('カテゴリ名が更新されました')
+        return redirect(url_for('main.category_maintenance'))
+    elif request.method == 'GET':
+        form.category.data = category.category
+    return render_template('category_update.html', form=form)
+
+
+@main.route('/<int:category_id>/delete_category', methods=['GET', 'POST'])
+@login_required
+def delete_category(category_id):
+    category = BlogCategory.query.get_or_404(category_id)
+    if not current_user.is_administrator():
+        abort(403)
+    db.session.delete(category)
+    db.session.commit()
+    flash('カテゴリが削除されました')
+    return redirect(url_for('main.category_maintenance'))
